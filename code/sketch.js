3691e2afcc72845d5bdba9faf1e4e880bd08dc9f
Blaze2305 <prajpran@gmail.com>
2019-06-07 20:27:40 +0530

    added image to be recreated : made sketch recreate image . can be changed when needed ( remove :rotation, translation, fill,stroke,sphere)

--- code/sketch.js
+++ code/sketch.js
@@ -8,12 +8,14 @@ let num=0;
 let button;
 let heights=[];
 
+
 function setup(){
   createCanvas(1000,1000,WEBGL);
   cols=floor(w/size);
   rows=floor(h/size);
   button=createButton("Press to freeze")
   button.mousePressed(freeze);
+
 
 }
 
@@ -25,27 +27,34 @@ function draw(){
 
   if(num==0){
 
-  for(i=0;i<=cols+1;i++){
-    heights[i]=[]
-    for(j=0;j<rows;j++){
-      if(i>cols/2){
-        heights[i][j]=map(noise(xoff,yoff),0,1,-100,200);
+    for(i=0;i<=cols+1;i++){
+      heights[i]=[]
+      for(j=0;j<rows;j++){
+        if(i>cols/2){
+          heights[i][j]=map(noise(xoff,yoff),0,1,-100,200);
+        }
+        else{
+          heights[i][j]=0
+        }
+        yoff+=0.1;
       }
-      else{
-        heights[i][j]=0
-      }
-      yoff+=0.1;
+      xoff+=0.1;
     }
-    xoff+=0.1;
+    start-=0.04
   }
-  start-=0.04
-}
   background(0);
-  stroke(255);
-  fill(100)
-  rotateX(PI/2.3)
+  stroke(50,232,244);
+  fill(118,40,170)
+  rotateX(PI/2.3+10*(PI/180));
   translate(-w/2,-h/2)
-
+  translate(100,0,0)
+ // 214,6,240
+ push();
+ noStroke()
+ fill(214,6,240)
+ translate(400,0,0)
+ sphere(250)
+ pop();
   for(i=0;i<cols;i++){
     beginShape(TRIANGLE_STRIP);
     for(j=0;j<rows;j++){
